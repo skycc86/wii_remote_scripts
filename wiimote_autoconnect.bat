80ba7e2d4394d453f40f4e74f1892d4413b9ebfd
skycc86 <skycc86@gmail.com>
2018-05-27 16:58:26 +0800

    Add files via upload
    src files

---
+++ wiimote_autoconnect.bat
@@ -0,0 +1,4 @@
+@echo off
+start "" "C:\Program Files\IVT Corporation\BlueSoleil\gprs.exe" "00:24:f3:57:6c:df" 4388
+start "" "C:\GlovePIE_0.43_en\GlovePIE.exe" "-C:\GlovePIE_0.43_en\my_wiimote_scripts\wii_gaming.PIE"
+rem start "" "C:\GlovePIE029\GlovePIE.exe" "-C:\GlovePIE029\my_wiimote_scripts\wii_gaming.PIE"
